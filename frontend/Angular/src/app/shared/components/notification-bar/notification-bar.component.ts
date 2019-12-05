48d74a4360cf4c937cc10fb5dacecc7f97f980cf
Lenkelheit <tymtsiv2710@gmail.com>
2019-12-05 12:36:09 +0200

    Add notifications

--- frontend/Angular/src/app/shared/components/notification-bar/notification-bar.component.ts
+++ frontend/Angular/src/app/shared/components/notification-bar/notification-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatSnackBarRef } from '@angular/material/snack-bar';
+import { Component, Inject } from '@angular/core';
+import { MatSnackBarRef, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-notification-bar',
@@ -8,5 +8,5 @@ import { MatSnackBarRef } from '@angular/material/snack-bar';
 })
 export class NotificationBarComponent {
 
-    constructor(public snackBarRef: MatSnackBarRef<NotificationBarComponent>) { }
+    constructor(public snackBarRef: MatSnackBarRef<NotificationBarComponent>, @Inject(MAT_SNACK_BAR_DATA) public data: any) { }
 }
